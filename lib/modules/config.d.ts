404f7f2659ca90a5d3994872c7c9dd10b819eabd
瑜笙 <2624367622@qq.com>
2025-07-28 12:54:53 +0800

    更新配置文件

--- lib/modules/config.d.ts
+++ lib/modules/config.d.ts
@@ -3,6 +3,9 @@ declare class Config {
     config: cfg | null;
     watcher: object | null;
     constructor();
+    init(): void;
+    /** 获取默认配置 */
+    get defcfg(): cfg;
     /** 获取配置 */
     get getcfg(): cfg;
     /** 获取项目根目录 */
